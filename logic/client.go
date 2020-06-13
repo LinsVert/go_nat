ffe00a93a6fe454eabdb1a9f51698d3b75c0535b
Lins <lindebiaoldb@gmail.com>
2020-06-13 17:59:55 +0800

    debug

--- logic/client.go
+++ logic/client.go
@@ -9,16 +9,19 @@ import (
 //基于本地转发 拓展的 nat 转发
 
 func main() {
-	conn, _ := net.Dial("tcp", "111.231.86.196:10086")
-	fmt.Println("dial conn success")
+	conn, _ := net.Dial("tcp", "127.0.0.1:10086")
+	fmt.Println("dial conn success", conn.RemoteAddr().String())
 	uR := make(chan []byte)
 	cR := make(chan []byte)
 	go read(conn, uR, false)
-	go write(conn, cR)
+	go write(conn, cR, false)
 	go ping(conn)
 	for {
+		//uR := make(chan []byte)
+		//cR := make(chan []byte)
+		//go read(conn, uR, false)
+		//go write(conn, cR, false)
 		run := make(chan bool)
-		fmt.Println("recv data", time.Now().String())
 		go handle(run, uR, cR)
 		<-run
 	}
@@ -34,12 +37,11 @@ func ping(conn net.Conn) {
 }
 func handle(run chan bool, uR chan []byte, bR chan []byte) {
 	var recv = make([]byte, 10240)
-	fmt.Println("handle in")
 	recv = <-uR
-	fmt.Println("handle out")
-	dial, _ := net.Dial("tcp", ":10084")
-	fmt.Println("dial 127.0.1:10084")
-	go write(dial, uR)
+	fmt.Println("recv data", time.Now().String())
+	dial, _ := net.Dial("tcp", ":8000")
+	fmt.Println("dial", dial.RemoteAddr().String())
+	go write(dial, uR, true)
 	go read(dial, bR, true)
 	uR <- recv
 	run <- true
@@ -53,7 +55,13 @@ func read(conn net.Conn, read chan []byte, isB bool) {
 	for {
 		var buf = make([]byte, 10240)
 		n, err := conn.Read(buf)
-		fmt.Println("read", conn.LocalAddr().String())
+		fmt.Println("read", conn.RemoteAddr().String(), n)
+		if n < 100 && n > 0 {
+			fmt.Println("Message", string(buf))
+		}
+		//if n == 0 {
+		//	continue
+		//}
 		if err != nil {
 			//if strings.Contains(err.Error(), "timeout") && !isHeart {
 			//	_ = conn.SetReadDeadline(time.Now().Add(time.Second * 3))
@@ -61,7 +69,7 @@ func read(conn net.Conn, read chan []byte, isB bool) {
 			//	continue
 			//}
 			fmt.Println(err.Error())
-			fmt.Println("close read")
+			fmt.Println("close read", isB)
 			_ = conn.Close()
 			break
 		}
@@ -72,13 +80,13 @@ func read(conn net.Conn, read chan []byte, isB bool) {
 	}
 }
 
-func write(conn net.Conn, write chan []byte) {
+func write(conn net.Conn, write chan []byte, isB bool) {
 	for {
 		var buf = make([]byte, 10240)
 		select {
 		case buf = <-write:
-			fmt.Println("write Data", conn.LocalAddr())
-			_ = conn.SetWriteDeadline(time.Now().Add(time.Second * 10))
+			//_ = conn.SetWriteDeadline(time.Now().Add(time.Second * 10))
+			fmt.Println("write Data", conn.RemoteAddr(), isB)
 			_, err := conn.Write(buf)
 			if err != nil {
 				fmt.Println("close write")
--- logic/server.go
+++ logic/server.go
@@ -8,56 +8,105 @@ import (
 
 func main() {
 	listen, _ := net.Listen("tcp", ":10086")
+	userL, _ := net.Listen("tcp", ":10087")
 	fmt.Println("start on 10086")
+	conn1, _ := listen.Accept()
+	fmt.Println("connect", time.Now().String())
+	//go handleS(userL, uR,sR)
+	//for {
+	//	uR := make(chan []byte)
+	//	sR := make(chan []byte)
+	//	//go test(conn)
+	//	go readS(conn, sR, false)
+	//	go writeS(conn, uR)
+	//	go handleS(userL, uR,sR)
+	//	//for {
+	//	//	uCon,_ := userL.Accept()
+	//	//	go readS(uCon, uR)
+	//	//	go writeS(uCon, sR)
+	//	//}
+	//}
+	uR := make(chan []byte)
+	sR := make(chan []byte)
+	go readS(conn1, sR, false)
+	go writeS(conn1, uR, false)
+	var connChan = make(chan net.Conn)
 	for {
-		conn, _ := listen.Accept()
-		uR := make(chan []byte)
-		fmt.Println("connect", time.Now().String())
-		go test(conn)
-		go readS(conn, uR)
-		go writeS(conn, uR)
+		//conn, _ := userL.Accept()
+		go getConn(userL, connChan)
+		//uRS := make(chan []byte)
+		//sRS := make(chan []byte)
+		var conn = <-connChan
+		fmt.Println("get conn", conn)
+
+		go readS(conn, uR, true)
+		go writeS(conn, sR, true)
+		//for {
+		//	time.Sleep(time.Second * 5)
+		//}
+
+		//go handleS(uR, sR, sRS, uRS)
 	}
 }
 
-func test(conn net.Conn) {
+func getConn(listener net.Listener, connChan chan net.Conn) {
+	conn, _ := listener.Accept()
+	connChan <- conn
+}
+
+func handleS(uR chan []byte, sR chan []byte, sRS chan []byte, uRS chan []byte) {
 	for {
-		time.Sleep(time.Second * 10)
-		_, _ = conn.Write([]byte("hello world on 10086"))
+		var buf = make([]byte, 10240)
+		select {
+		case buf = <-sR:
+			sRS <- buf
+		case buf = <-uRS:
+			uR <- buf
+		}
 	}
 }
 
-func readS(conn net.Conn, read chan []byte) {
-	//_ = conn.SetReadDeadline(time.Now().Add(time.Second * 10))
+func readS(conn net.Conn, read chan []byte, isU bool) {
 	for {
+		if isU {
+			//_ = conn.SetReadDeadline(time.Now().Add(time.Second * 10))
+		}
 		var buf = make([]byte, 10240)
 		n, err := conn.Read(buf)
 		fmt.Println("read", conn.LocalAddr().String())
+		if n < 100 && n > 0 {
+			fmt.Println("Message", string(buf))
+		}
 		if err != nil {
-			fmt.Println("close read")
+			fmt.Println("close read", isU)
 			_ = conn.Close()
 			break
 		}
-
+		if string(buf[:4]) == "ping" {
+			_, _ = conn.Write([]byte("pong " + time.Now().String()))
+			continue
+		}
+		fmt.Println(n, "read at", isU)
 		read <- buf[:n]
 	}
 }
 
-func writeS(conn net.Conn, read chan []byte) {
+func writeS(conn net.Conn, read chan []byte, isU bool) {
 	for {
 		var buf = make([]byte, 10240)
 		select {
 		case buf = <-read:
-			if string(buf) == "ping" {
-				_, err := conn.Write([]byte("pong " + time.Now().String()))
-				if err != nil {
-					fmt.Println("close write")
-					_ = conn.Close()
-					break
-				}
-			} else {
-				fmt.Println(string(buf))
+			n, err := conn.Write(buf)
+			if n < 100 && n > 0 {
+				fmt.Println("Message write", string(buf))
 			}
-
+			fmt.Println(n, "write at", isU)
+			if err != nil {
+				fmt.Println("close write2")
+				_ = conn.Close()
+				break
+			}
 		}
+
 	}
 }
